398999b47bee78de5c4fae29f54b96d25ac7c795
Agata Liberska <liberska@gmail.com>
2021-06-09 13:05:01 +0100

    create job list component

---
+++ src/components/JobList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import jobs from '../jobs.json';
+
+class JobList extends React.Component {
+    render() {
+        <div>Job list</div>
+    }
+}
+
+export default JobList;
